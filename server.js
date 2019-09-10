8d9db9e3920c67534357ac731e04111a5bd07228
chelle tuerk <michelletuerk@gmail.com>
2019-09-10 17:12:19 -0600

    initial commit

--- server.js
+++ server.js
@@ -12,14 +12,14 @@ app.get('/', function (req, res) {
 app.listen(3000)
 
 
-const accountSid = 'ACe3c85733d16ac48b099c8916e09fb56e'
-const authToken = '1b4ffa2410ff54be2e4b50bc1b010f86'
+const accountSid = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+const authToken = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 const client = require('twilio')(accountSid, authToken)
 
 client.messages
   .create({
      body: 'gonna ride the ol test train again',
-     from: '+19704708659',
-     to: '+13035477403'
+     from: '+xxxxxxxxxxx',
+     to: '+xxxxxxxxxxx'
    })
   .then(message => console.log(message.sid))
